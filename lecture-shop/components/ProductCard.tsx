b63b1828f23a43ce97724f287ec9034a4fd22efc
Thomsen97 <sebastwt@hiof.no>
2025-09-04 09:09:20 +0200

    d

---
+++ lecture-shop/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import {View} from 'react-native';
+import ProductTitle from './ProductTitle';
+import ProductPrice from './ProductPrice';
+
+
+export default function ProductCard() {
+    <View>
+        <ProductTitle />
+        <ProductPrice />
+    </View>
+}
